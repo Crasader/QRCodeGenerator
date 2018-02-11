2122d28d45172f6e7a537476208356ae8025d8fe
chenzhuowei <chenzhuowei@xianjinxia.com>
2018-02-11 14:19:33 +0800

    二维码生成逻辑

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -8,106 +8,58 @@ Scene* HelloWorld::createScene()
     return HelloWorld::create();
 }
 
-// Print useful error message instead of segfaulting when files are not there.
-static void problemLoading(const char* filename)
-{
-    printf("Error while loading: %s\n", filename);
-    printf("Depending on how you compiled you might have to add 'Resources/' in front of filenames in HelloWorldScene.cpp\n");
-}
-
-// on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !Scene::init() )
-    {
+    if ( !Scene::init() ) {
         return false;
     }
 
-    auto visibleSize = Director::getInstance()->getVisibleSize();
-    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+	bool bRet = m_QRCoder.EncodeData(0, 0, 1, -1, "https://www.baidu.com/");
+	if (bRet) {
+		int nSize = 5;
+		int originalSize = m_QRCoder.m_nSymbleSize + (QR_MARGIN * 2);
+		CCDrawNode *pQRNode = CCDrawNode::create();
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
+		CCPoint pt[6];
+		ccColor4F color;
 
-    // add a "close" icon to exit the progress. it's an autorelease object
-    auto closeItem = MenuItemImage::create(
-                                           "CloseNormal.png",
-                                           "CloseSelected.png",
-                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+		pt[0] = ccp(0, 0);
+		pt[1] = ccp((m_QRCoder.m_nSymbleSize + QR_MARGIN * 2)*nSize, (m_QRCoder.m_nSymbleSize + QR_MARGIN * 2)*nSize);
+		pt[2] = ccp((m_QRCoder.m_nSymbleSize + QR_MARGIN * 2)*nSize, 0);
 
-    if (closeItem == nullptr ||
-        closeItem->getContentSize().width <= 0 ||
-        closeItem->getContentSize().height <= 0)
-    {
-        problemLoading("'CloseNormal.png' and 'CloseSelected.png'");
-    }
-    else
-    {
-        float x = origin.x + visibleSize.width - closeItem->getContentSize().width/2;
-        float y = origin.y + closeItem->getContentSize().height/2;
-        closeItem->setPosition(Vec2(x,y));
-    }
+		pt[3] = pt[0];
+		pt[4] = ccp(0, (m_QRCoder.m_nSymbleSize + QR_MARGIN * 2)*nSize);
+		pt[5] = pt[1];
+		color = ccc4f(1, 1, 1, 1);
+		pQRNode->drawPolygon(pt, 6, color, 0, color);
 
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(closeItem, NULL);
-    menu->setPosition(Vec2::ZERO);
-    this->addChild(menu, 1);
+		for (int i = 0; i < m_QRCoder.m_nSymbleSize; ++i)
+		{
+			for (int j = 0; j < m_QRCoder.m_nSymbleSize; ++j)
+			{
+				pt[0] = ccp((i + QR_MARGIN)*nSize, (j + QR_MARGIN)*nSize);
+				pt[1] = ccp(((i + QR_MARGIN) + 1)*nSize, ((j + QR_MARGIN) + 1)*nSize);
+				pt[2] = ccp(((i + QR_MARGIN) + 1)*nSize, ((j + QR_MARGIN) + 0)*nSize);
 
-    /////////////////////////////
-    // 3. add your codes below...
+				pt[3] = pt[0];
+				pt[4] = ccp(((i + QR_MARGIN) + 0)*nSize, ((j + QR_MARGIN) + 1)*nSize);
+				pt[5] = pt[1];
+				if (m_QRCoder.m_byModuleData[i][j] == 1)
+				{
+					color = ccc4f(0, 0, 0, 1);
+				}
+				else
+				{
+					color = ccc4f(1, 1, 1, 1);
+				}
+				pQRNode->drawPolygon(pt, 6, color, 0, color);
+			}
+		}
+		CCSize winSize = CCDirector::sharedDirector()->getWinSize();
+		pQRNode->setPosition(ccp((winSize.width - nSize * m_QRCoder.m_nSymbleSize) / 2, winSize.height - (winSize.height - nSize * m_QRCoder.m_nSymbleSize) / 2));
+		pQRNode->setScaleY(-1);
+		addChild(pQRNode);
+	}
 
-    // add a label shows "Hello World"
-    // create and initialize a label
-
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
-    if (label == nullptr)
-    {
-        problemLoading("'fonts/Marker Felt.ttf'");
-    }
-    else
-    {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                                origin.y + visibleSize.height - label->getContentSize().height));
-
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
-
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
     return true;
-}
-
-
-void HelloWorld::menuCloseCallback(Ref* pSender)
-{
-    //Close the cocos2d-x game scene and quit the application
-    Director::getInstance()->end();
-
-    #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
-
-    /*To navigate back to native iOS screen(if present) without quitting the application  ,do not use Director::getInstance()->end() and exit(0) as given above,instead trigger a custom event created in RootViewController.mm as below*/
-
-    //EventCustom customEndEvent("game_scene_close_event");
-    //_eventDispatcher->dispatchEvent(&customEndEvent);
-
-
 }
